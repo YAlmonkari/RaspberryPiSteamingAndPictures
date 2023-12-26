6ec3bc932346398a279cfd2853adce3069719a5f
YAlmonkari <36625605+YAlmonkari@users.noreply.github.com>
2023-12-26 13:10:53 +0100

    Update camera4b+BnW.sh

--- camera4b+BnW.sh
+++ camera4b+BnW.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d_%H%M%S")
-rpicam-still -q 100 -r --saturation 0.0 -o /home/pi/Pictures/$DATE.jpg --width 3280 --height 2464
+rpicam-still -q 100 -r --shutter 1000000--saturation 0.0 -o /home/pi/Pictures/$DATE.jpg --width 3280 --height 2464
