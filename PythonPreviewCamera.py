94b2ff6dd976ee92856fd5a55ccc9bcd0e266c84
Yrjö Almonkari <36625605+YAlmonkari@users.noreply.github.com>
2023-12-27 16:05:30 +0100

    Update PythonPreviewCamera.py

--- PythonPreviewCamera.py
+++ PythonPreviewCamera.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Dec 26 16:20:44 2023
-
 @author: Yrjö Almonkari
 """
-
 from picamera import PiCamera
-from time import sleep
 import time
 date = time.strftime("%Y-%m-%d_%H%M%S")
 camera = PiCamera()
+camera.iso = 800
+#0,6s exposure
+camera.shutter_speed = 600000
 camera.start_preview()
-sleep(5)
+time.sleep(5)
 camera.resolution = (2592, 1944)
 camera.capture('/home/pi/Pictures/' + date + '.jpg')
 camera.stop_preview()
